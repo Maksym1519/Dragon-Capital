2bf12562c0afdb59b3c98d0001d9a90556107116
Maksym Korostelov <flying1519@gmail.com>
2023-08-15 17:34:11 +0300

    start project

---
+++ src/components/pages/Main/Main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { lazy, Suspense } from "react";
+import { Link, Route, Router, Routes } from "react-router-dom";
+import m from "./main.module.scss";
+import { useRef, useState } from "react";
+import { useEffect } from "react";
+import { register } from "swiper/element/bundle";
+
+
+const Main = () => {
+ 
+  return (
+    <div className={m.main__wrapper}>
+  
+    </div>
+  );
+};
+export default Main;
