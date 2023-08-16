3003bcd9fbd1855e18df772cd5a15ac1de1993ff
Pavlo <tishkovets.pavlo@gmail.com>
2023-08-16 17:48:31 +0300

    departments done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { lazy, Suspense } from "react";
 import loadable from "@loadable/component";
 import { BrowserRouter, Routes, Route, Link, Switch } from "react-router-dom";
 const MainLazy = React.lazy(() => import("./components/pages/Main/Main"));
+const DepartmentsLazy = React.lazy(() =>import("./components/pages/Departments/Departments.jsx"));
 
 import "./fonts.scss";
 import "./style.scss";
@@ -20,6 +21,14 @@ const App = () => {
           element={
             <React.Suspense>
               <MainLazy />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="/departments"
+          element={
+            <React.Suspense>
+              <DepartmentsLazy />
             </React.Suspense>
           }
         />
---
+++ src/components/atoms/Text14700.jsx
@@ -0,0 +1,11 @@
+import t from './text.module.scss';
+
+const Text14700 = (props) => {
+    return (
+        <p className={t.text14700} style={props.color}>
+            {props.text}
+        </p>
+    )
+}
+
+export default Text14700;
